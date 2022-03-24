fdb59decd56b154e1245679dbcd7ef97f35a50da
Pablovv56 <68592234+Pablovv56@users.noreply.github.com>
2022-03-24 16:21:59 +0100

    Create Laberinto.java

---
+++ Laberinto.java
@@ -0,0 +1,16 @@
+public class Laberinto {
+	private int x, y;
+	private char [][] matriz;
+	private int prob;
+	private int iniX, iniY, finX, finY;
+	
+	public Laberinto (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void generarLaberinto () {
+		
+	}
+
+}
