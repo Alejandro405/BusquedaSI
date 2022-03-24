90e84747a29d213efbff305f8a9333880989f847
Pablovv56 <68592234+Pablovv56@users.noreply.github.com>
2022-03-24 17:10:07 +0100

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo {
+
+    private int corX;
+    private int corY;
+    private int coste;
+    private Nodo padre;
+
+    public Nodo (int cordX, int cordY, Nodo padre){
+        this.corX=cordX;
+        this.corY=cordY;
+
+    }
+}
