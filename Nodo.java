8b3d9f225698d08bbd7a6e726ad1659ef859f233
Alejandro405 <80174674+Alejandro405@users.noreply.github.com>
2022-03-29 15:57:34 +0200

    Nodo comparable

--- Nodo.java
+++ Nodo.java
@@ -1,13 +1,23 @@
-public class Nodo {
+public class Nodo implements Comparable<Nodo> {
 
     private int corX;
     private int corY;
-    private int coste;
+
+    private int g;
+    private int h;
+    private int f;
+
     private Nodo padre;
 
     public Nodo (int cordX, int cordY, Nodo padre){
         this.corX=cordX;
         this.corY=cordY;
 
+    }
+
+    @Override
+    public int compareTo(Nodo o) {
+        // TODO Auto-generated method stub
+        return 0;
     }
 }
