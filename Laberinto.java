eec7aa3c9327c71c5bde0c4a8023ecbc3161f055
Pablovv56 <68592234+Pablovv56@users.noreply.github.com>
2022-03-29 15:44:49 +0200

    Clase principal

--- Laberinto.java
+++ Laberinto.java
@@ -9,23 +9,31 @@ public class Laberinto {
 
 	private int dimensionX, dimensionY;
 	private char[][] matriz;
-	private int prob;
+	private int prob = 30;
 	private int iniX, iniY, finX, finY;
 	private long seed;
 	
 	public Laberinto (int x, int y) {
 		this.dimensionX = x;
 		this.dimensionY = y;
+        matriz = new char [dimensionX][dimensionY];
+        for (int i = 0; i < dimensionX; i++) {
+            for (int j = 0; j < dimensionY; j++) {
+                matriz[i][j] = LIBRE;
+            }
+        }
 	}
 	
 	public void generarLaberinto () {
 		Random pos = new Random(seed); 
         int tamTotal = dimensionX * dimensionY;
-        int numObstculos = tamTotal * (prob/100);
+        int numObstculos = (int)(tamTotal * (prob/100.0));
         int obsX, obsY;
 
         iniX = pos.nextInt(dimensionX-1);
         iniY = pos.nextInt(dimensionY-1);
+        matriz [iniX][iniY] = SALIDA;
+
         finX = pos.nextInt(dimensionX-1);
         finY = pos.nextInt(dimensionY-1);
 
@@ -33,6 +41,7 @@ public class Laberinto {
             finX = pos.nextInt(dimensionX-1);
             finY = pos.nextInt(dimensionY-1);
         }
+        matriz [finX][finY] = FIN;
         
         for (int i = 0; i < numObstculos; i++) {
             do {
@@ -48,13 +57,19 @@ public class Laberinto {
         if(matriz[x][y] == LIBRE) {
             return true;
         }else{
-            return false;
+             return false;
         }
     }
 
     public void setProbabilidad (int newProb) {
         prob = newProb;
 		//actualizarTablero();
+    }
+
+    public void mostrarLaberinto () {
+
+        System.out.println(this.toString());
+      
     }
 
     public int getProbabilidad () {
@@ -68,7 +83,7 @@ public class Laberinto {
 		for (int i = 0; i < dimensionX; i++) {
             String linea = "";
             for (int j = 0; j < dimensionY; j++) {
-                linea += matriz[i][j];
+                linea += "[" + matriz[i][j] + "]";
             }
 			res.add(linea);
 		}
@@ -77,3 +92,4 @@ public class Laberinto {
 	}
 
 }
+
